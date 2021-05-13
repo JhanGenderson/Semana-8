5f5b43b8bd0e21dd82a4fe08ea8398eb004747a3
JhanGenderson <jhan96.rc@gmail.com>
2021-05-13 18:24:40 -0500

    update

---
+++ Dia1/04-asincronia.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    console.log("1. Horneamos la Torta")
+  }, 4000);
+  
+  setTimeout(function(){
+    console.log("2. Ponemos el Chocolate al microondas")
+  }, 3000);
+  
+  console.log("3. Preparo la torta con el chocolate y se decora")
