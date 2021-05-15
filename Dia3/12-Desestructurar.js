6df3919aec3d3b5de0824561bea173e0a5e80b2f
JhanGenderson <jhan96.rc@gmail.com>
2021-05-15 15:23:09 -0500

    update

---
+++ Dia3/12-Desestructurar.js
@@ -0,0 +1,9 @@
+let objVehiculo = {
+    marca:"Tesla",
+    modelo:"Model X",
+    anio:2019,
+    colores:["Rojo", "Gris"],
+    prender: function(){
+        console.log("Bzzzzzz Bzzzz")
+    }
+}
