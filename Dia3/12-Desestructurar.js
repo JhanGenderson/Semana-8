2e729486c4bb93c8d338a665bb5c13082db7f0a2
JhanGenderson <jhan96.rc@gmail.com>
2021-05-15 19:09:14 -0500

    update

--- Dia3/12-Desestructurar.js
+++ Dia3/12-Desestructurar.js
@@ -4,6 +4,55 @@ let objVehiculo = {
     anio:2019,
     colores:["Rojo", "Gris"],
     prender: function(){
-        console.log("Bzzzzzz Bzzzz")
+      console.log("bzzzzzz bzzzzzzzz")
     }
-}
+  }
+  //TRADICIONAL, ORTODOXO, CLÁSICO
+  // console.log(objVehiculo.marca)
+  // console.log(objVehiculo.modelo)
+  // objVehiculo.prender()
+  
+  let {marca, modelo, anio, colores, prender} = objVehiculo;
+  
+  console.log(marca);
+  console.log(modelo);
+  
+  prender();
+  
+  console.log(anio + 2);
+  
+  //Buena práctica con desestructuración
+  
+  // let calcularBeneficios = (sueldo, vacaciones, bono1, bono2, cts, gratificación) => {
+  //   //suma de cada uno, restar impuestos
+  // }
+  
+  // calcularBeneficios(1600, 600, 400, 350, 1100, 500);
+  
+  let calcularBeneficios = ({sueldo, vacaciones, bono1, bono2, cts, gratificacion}) => {
+    //suma de cada uno, restar impuestos
+    //objACalcular.sueldo
+    //objACalcular.vacaciones
+  
+    console.log(sueldo)
+  }
+  
+  let objBeneficios = {
+    sueldo:1600,
+    vacaciones:600,
+    gratificacion:500,
+    bono1:400,
+    bono2:350,
+    cts:1100,
+  }
+  
+  calcularBeneficios(objBeneficios);
+
+//   Desestructurar Arreglos
+let nombres =["Linder","Daniel","Joel","Lezly","Jorgito"];
+
+let prosor = nombres[4];
+
+console.log(prosor);
+let [tutor2, boss, teacher, tutora, prosorcito];
+console.log(tutora)
---
+++ Dia4/js/app.js
@@ -0,0 +1,5 @@
+// App.js es el script principal
+// 1. Tener MockAPI listo
+// 2. Obtener los datos (GET)
+// 3. Convertir esos datos a Elementos de HTML
+// 4. Crear desde el navegador 
